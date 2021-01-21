f67d78ed30e50af0cd0cb47b97582294ba1e5fbe
dengxinggao <35987500+dengxinggao@users.noreply.github.com>
2021-01-21 11:32:08 +0800

    Update diy-part2.sh
    config wndr4300 128m flash

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/^wndr4300_mtdlayout.*$/wndr4300_mtdlayout=mtdparts=ar934x-nfc:256k(u-boot)ro,256k(u-boot-env)ro,256k(caldata),512k(pot),2048k(language),512k(config),3072k(traffic_meter),2048k(kernel),121856k(ubi),123904k@0x6c0000(firmware),256k(caldata_backup),-(reserved)/' target/linux/ar71xx/image/legacy.mk
+
